a825057e00ac999c56675979878329e4592231f4
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-04-11 18:42:41 -0500

    Testing random policy

--- agent.py
+++ agent.py
@@ -1,13 +1,15 @@
 # class for agent
 class Agent():
 
-  state_space = [0, 4, 0]
+  past_state_space = [0, 4, 0]
+  new_state_space = [0, 4, 0]
   policy = None
   bank_account = 0
+  reward = 0
   num_operators = 0
 
   def __init__(self):
-    state_space = [1, 5, 0] #[row, column, block]
+    state_space = [0, 4, 0] #[row, column, block]
     policy = None
     bank_account = 0
     num_operators = 0
--- pd_main.py
+++ pd_main.py
@@ -7,13 +7,11 @@ pickup_matrix = [[[None, -1, -1, None], [None, -1, -1, 13], [None, -1, -1, -1], 
                  [[-1, -1, -1, None], [-1, -1, -1, -1], [13, -1, -1, -1], [-1, -1, -1, -1], [-1, None, 13, -1]],
                  [[-1, -1, None, None], [-1, -1, None, -1], [-1, -1, None, -1], [-1, 13, None, -1], [-1, None, None, -1]]]
 
-q_table = [ [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0], [0,0,0,0,0,0],
-            [0,0,0,0,0,0]]
+q_table = [ [0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],
+            [0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],
+            [0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],
+            [0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],
+            [0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
 
 directions = {
         0 : 'north',
@@ -30,15 +28,16 @@ discount_rate = None
 agent = Agent()
 agent.policy = "PRandom"
 
-row = agent.state_space[0]
-col = agent.state_space[1]
-hasBlock = agent.state_space[2]
+row = agent.new_state_space[0]
+col = agent.new_state_space[1]
+hasBlock = agent.new_state_space[2]
 position = [row, col]
 
 # TODO: pass in row to lookup in q-table
 def PRandom(row, col, agent):
     if (pickup_matrix[row][col] in pickup_states):
       agent.state_space[2] = 1       # pickup
+      agent.bank_account += 13
       # TODO: decrement blocks on space
     else:
       possible_actions = []
@@ -59,6 +58,15 @@ def PRandom(row, col, agent):
       elif direction == "west":
         agent.state_space[1] -= 1
 
-# print("Current position: ", agent.state_space)
-# PRandom(row,col,agent)
-# print("New position: ", agent.state_space)
+      agent.bank_account -= 1
+
+    agent.num_operators += 1
+# end PRandom
+
+# TODO: update Q-value
+# def Q_learningUpdate():
+
+
+print("Current position: ", agent.new_state_space)
+PRandom(row,col,agent)
+print("New position: ", agent.new_state_space)
