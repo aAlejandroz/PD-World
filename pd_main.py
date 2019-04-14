5a44033211537206fd4c17cd4ec72ed1d290b43a
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-04-14 17:23:07 -0500

    we still tryna refactor

--- pd_main.py
+++ pd_main.py
@@ -1,5 +1,7 @@
 from agent import Agent
-from policies import Policies
+from policies import *
+from cell_methods import *
+from RL_updates import *
 from grid_cells import Cells
 import random
 import copy
@@ -33,38 +35,9 @@ dropoff_q_table = [ [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,
             [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]],
             [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]]
 
-pickup_cells = []   # list of pickup cells
-dropoff_cells = []  # list of drop off cells
-
 agent = Agent()
 
-# ------------------- DECREMENT/INCREMENT BLOCK COUNT ON CELL ------------------- #
-
-# decrement blocks on cell #
-def decrementNumBlocksInCell(pos):
-  cell = getCellFromPosition(pos, pickup_cells)
-  cell.num_of_blocks -= 1
-
-
-# increment blocks on cell #
-def incrementNumBlocksInCell(pos):
-  cell = getCellFromPosition(pos, dropoff_cells)
-  cell.num_of_blocks += 1
-
-
 # ------------------------------ HELPER FUNCTIONS ------------------------------- #
-
-# Initialize environment to original #
-def initalizeCells(pickup_states, dropoff_states):
-  pickup_cells.clear()
-  dropoff_cells.clear()
-
-  for pos in pickup_states:
-    pickup_cells.append(Cells(pos, 5))
-
-  for pos in dropoff_states:
-    dropoff_cells.append(Cells(pos, 0))
-
 
 # calculates reward from action #
 def calculateRewardFromAction(action):
@@ -79,12 +52,6 @@ def calculateRewardFromAction(action):
     reward = 13
 
   return reward
-
-# returns the cell object in the given position #
-def getCellFromPosition(pos, cell_list):
-  for cell in cell_list:
-    if cell.position == pos:
-      return cell
 
 
 # returns action enum given agent's policy and possible actions #
@@ -154,78 +121,6 @@ def getAllPossibleNextAction(position):
       possible_actions.append(i)
 
   return (possible_actions)
-
-# ------------------------- REINFORCEMENT LEARNING UPDATES ------------------------- #
-
-# Q Learning update #
-def Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states):
-  position = agent.position
-  row = position[0]
-  col = position[1]
-
-  possible_actions = getAllPossibleNextAction(position)                                          # possible actions in state
-  action = getPolicyAction(agent, agent.state, possible_actions, pickup_states, dropoff_states)  # a = action chosen in state
-  next_state = getNextState(agent.state, action)                                                 # s' = next state after action is applied
-  reward = calculateRewardFromAction(action)                                                     # calculate agent's rewards & bank account
-  agent.updateRewards(reward)
-
-  new_row = next_state[0]
-  new_col = next_state[1]
-
-  q_table = dropoff_q_table if agent.hasBlock() else pickup_q_table
-
-  old_value = q_table[row][col][action.value]
-  next_max = np.max(q_table[new_row][new_col])
-  new_q_value = (1 - learning_rate) * old_value + learning_rate * (agent.reward + discount_rate * next_max)
-  q_table[row][col][action.value] = new_q_value
-
-  agent.updateState(next_state)
-  agent.updatePosition()
-
-  is_terminal = False
-  val = 0
-
-  for cell in dropoff_cells:
-    val += cell.num_of_blocks
-    if val == 15:
-      is_terminal = True
-
-  if is_terminal:
-    agent.initialize()
-    initalizeCells(pickup_states, dropoff_states)
-    print("\n-----------INITIALIZED----------")
-
-
-
-# SARSA update, returns next action #
-def SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states):
-  position = agent.position
-  row = position[0]
-  col = position[1]
-
-  if next_action is None:
-    possible_actions = getAllPossibleNextAction(position)                                                  # possible actions in state
-    action = getPolicyAction(agent, agent.state, possible_actions, pickup_states, dropoff_states)          # a = action chosen in state
-  else:
-    action = next_action                                                    # we know what our action is, so we chose it
-
-  reward = calculateRewardFromAction(action)                                # calculate agent's rewards & bank account
-  agent.updateRewards(reward)
-
-  next_state = getNextState(agent.state, action)                            # s' = next state after action is applied
-  next_possible_actions = getAllPossibleNextAction(next_state)              # all possible actions in s'
-  next_action = getPolicyAction(agent, next_state, next_possible_actions, pickup_states, dropoff_states)   # a' = next action in s'
-
-  q_table = dropoff_q_table if agent.hasBlock() else pickup_q_table
-  q_value = q_table[row][col][action.value]
-
-  q_table[row][col][action.value] = q_value + learning_rate * (reward + discount_rate *
-                                                                q_table[next_state[0]][next_state[1]][next_action.value] - q_value)
-
-  agent.updateState(next_state)
-  agent.updatePosition()
-
-  return next_action
 
 
 # ----------------------------- EXPERIMENTS ---------------------------- #
