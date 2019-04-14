416f0109ce06cc67494f8fa56c56300b87c59808
Phillip Nguyen <phillipnguyen0421@gmail.com>
2019-04-14 16:58:49 -0500

    Agent and world reinitialization and policy class

--- pd_main.py
+++ pd_main.py
@@ -1,4 +1,5 @@
 from agent import Agent
+from policies import Policies
 from grid_cells import Cells
 import random
 import copy
@@ -32,21 +33,10 @@ dropoff_q_table = [ [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,
             [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]],
             [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]]
 
-pickup_states = [[0, 0], [2, 2], [4, 4]]
-dropoff_states = [[1, 4], [4, 0], [4, 2]]
 pickup_cells = []   # list of pickup cells
 dropoff_cells = []  # list of drop off cells
 
-for pos in pickup_states:
-  pickup_cells.append(Cells(pos, 5))
-
-for pos in dropoff_states:
-  dropoff_cells.append(Cells(pos, 0))
-
 agent = Agent()
-
-learning_rate = 0.5
-discount_rate = 1
 
 # ------------------- DECREMENT/INCREMENT BLOCK COUNT ON CELL ------------------- #
 
@@ -62,64 +52,19 @@ def incrementNumBlocksInCell(pos):
   cell.num_of_blocks += 1
 
 
-# --------------------------------- POLICIES --------------------------------- #
-
-# random policy #
-def PRandom(possible_actions):
-  return random.choice(possible_actions)
-
-
-# exploit policy #
-def PExploit(possible_actions, agent, row, col):
-  duplicate = []
-
-  q_table = dropoff_q_table if agent.hasBlock() else pickup_q_table
-
-  # choose action with best q-value 80% of the time
-  if random.random() <= 0.8:
-    max_action = possible_actions[0]
-    for num in possible_actions:
-      q_value = q_table[row][col][num]
-      max_q_value = q_table[row][col][max_action]
-
-      if q_value > max_q_value:
-        max_action = num
-        duplicate.clear()
-        duplicate.append(num)
-      if q_value == max_q_value:
-        duplicate.append(num)
-
-    exploit_choice = random.choice(duplicate) if len(duplicate) > 1 else max_action
-  else:
-    exploit_choice = random.choice(possible_actions)
-
-  return exploit_choice
-
-
-# greedy policy #
-def PGreedy(possible_actions, agent, row, col):
-  duplicate = []
-
-  q_table = dropoff_q_table if agent.hasBlock() else pickup_q_table
-
-  # choose action with best q-value 100% of the time
-  max_action = possible_actions[0]
-  for num in possible_actions:
-    q_value = q_table[row][col][num]
-    max_q_value = q_table[row][col][max_action]
-
-    if q_value > max_q_value:
-      max_action = num
-      duplicate.clear()
-      duplicate.append(num)
-    if q_value == max_q_value:
-      duplicate.append(num)
-
-  greedy_choice = random.choice(duplicate) if len(duplicate) > 1 else max_action
-
-  return greedy_choice
-
 # ------------------------------ HELPER FUNCTIONS ------------------------------- #
+
+# Initialize environment to original #
+def initalizeCells(pickup_states, dropoff_states):
+  pickup_cells.clear()
+  dropoff_cells.clear()
+
+  for pos in pickup_states:
+    pickup_cells.append(Cells(pos, 5))
+
+  for pos in dropoff_states:
+    dropoff_cells.append(Cells(pos, 0))
+
 
 # calculates reward from action #
 def calculateRewardFromAction(action):
@@ -135,7 +80,7 @@ def calculateRewardFromAction(action):
 
   return reward
 
-# returns the cell object in the given position
+# returns the cell object in the given position #
 def getCellFromPosition(pos, cell_list):
   for cell in cell_list:
     if cell.position == pos:
@@ -143,7 +88,7 @@ def getCellFromPosition(pos, cell_list):
 
 
 # returns action enum given agent's policy and possible actions #
-def getPolicyAction(agent, state, possible_actions):
+def getPolicyAction(agent, state, possible_actions, pickup_states, dropoff_states):
   row = state[0]
   col = state[1]
   pos = [row, col]
@@ -213,15 +158,15 @@ def getAllPossibleNextAction(position):
 # ------------------------- REINFORCEMENT LEARNING UPDATES ------------------------- #
 
 # Q Learning update #
-def Q_learning(learning_rate, discount_rate, agent):
+def Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states):
   position = agent.position
   row = position[0]
   col = position[1]
 
-  possible_actions = getAllPossibleNextAction(position)           # possible actions in state
-  action = getPolicyAction(agent, agent.state, possible_actions)  # a = action chosen in state
-  next_state = getNextState(agent.state, action)                  # s' = next state after action is applied
-  reward = calculateRewardFromAction(action)                      # calculate agent's rewards & bank account
+  possible_actions = getAllPossibleNextAction(position)                                          # possible actions in state
+  action = getPolicyAction(agent, agent.state, possible_actions, pickup_states, dropoff_states)  # a = action chosen in state
+  next_state = getNextState(agent.state, action)                                                 # s' = next state after action is applied
+  reward = calculateRewardFromAction(action)                                                     # calculate agent's rewards & bank account
   agent.updateRewards(reward)
 
   new_row = next_state[0]
@@ -234,21 +179,33 @@ def Q_learning(learning_rate, discount_rate, agent):
   new_q_value = (1 - learning_rate) * old_value + learning_rate * (agent.reward + discount_rate * next_max)
   q_table[row][col][action.value] = new_q_value
 
-  #print(f'Q({action.name}, {agent.state}) = {pickup_q_table[row][col][action.value]}')
-
   agent.updateState(next_state)
   agent.updatePosition()
 
+  is_terminal = False
+  val = 0
+
+  for cell in dropoff_cells:
+    val += cell.num_of_blocks
+    if val == 15:
+      is_terminal = True
+
+  if is_terminal:
+    agent.initialize()
+    initalizeCells(pickup_states, dropoff_states)
+    print("\n-----------INITIALIZED----------")
+
+
 
 # SARSA update, returns next action #
-def SARSA_update(learning_rate, discount_rate, next_action, agent):
+def SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states):
   position = agent.position
   row = position[0]
   col = position[1]
 
   if next_action is None:
-    possible_actions = getAllPossibleNextAction(position)                   # possible actions in state
-    action = getPolicyAction(agent, agent.state, possible_actions)          # a = action chosen in state
+    possible_actions = getAllPossibleNextAction(position)                                                  # possible actions in state
+    action = getPolicyAction(agent, agent.state, possible_actions, pickup_states, dropoff_states)          # a = action chosen in state
   else:
     action = next_action                                                    # we know what our action is, so we chose it
 
@@ -257,7 +214,7 @@ def SARSA_update(learning_rate, discount_rate, next_action, agent):
 
   next_state = getNextState(agent.state, action)                            # s' = next state after action is applied
   next_possible_actions = getAllPossibleNextAction(next_state)              # all possible actions in s'
-  next_action = getPolicyAction(agent, next_state, next_possible_actions)   # a' = next action in s'
+  next_action = getPolicyAction(agent, next_state, next_possible_actions, pickup_states, dropoff_states)   # a' = next action in s'
 
   q_table = dropoff_q_table if agent.hasBlock() else pickup_q_table
   q_value = q_table[row][col][action.value]
@@ -270,21 +227,27 @@ def SARSA_update(learning_rate, discount_rate, next_action, agent):
 
   return next_action
 
+
 # ----------------------------- EXPERIMENTS ---------------------------- #
 
 def experiment_1():
   learning_rate = 0.3
   discount_rate = 0.5
 
+  pickup_states = [[0, 0], [2, 2], [4, 4]]
+  dropoff_states = [[1, 4], [4, 0], [4, 2]]
+
+  initalizeCells(pickup_states, dropoff_states)
+
   for index in range(4000):
     agent.policy = "PRandom"
-    Q_learning(learning_rate, discount_rate, agent)
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
 
   #TODO Where we display the Q_table
 
   for index in range(4000):
     agent.policy = "PGreedy"
-    Q_learning(learning_rate, discount_rate, agent)
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
 
   print('\n')
 
@@ -300,42 +263,46 @@ def experiment_1():
       print(dropoff_q_table[row][column], end=" ")
     print()
 
-  print("\nPickup states: ")
-  for i in range(3):
-    print("pickup state ", i, ": ", pickup_cells[i].num_of_blocks)
-
-  print("\nDropoff states: ")
-  for i in range(3):
-    print("Dropoff state ", i, ": ", dropoff_cells[i].num_of_blocks)
 
 def experiment_2():
   learning_rate = 0.3
   discount_rate = 0.5
 
+  pickup_states = [[0, 0], [2, 2], [4, 4]]
+  dropoff_states = [[1, 4], [4, 0], [4, 2]]
+
+  initalizeCells(pickup_states, dropoff_states)
+
   for index in range(200):
     agent.policy = "PRandom"
-    Q_learning(learning_rate,discount_rate,agent)
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
 
   # TODO Display and interpret the Q-table
 
   for index in range(7800):
     agent.policy = "PExploit"
-    Q_learning(learning_rate,discount_rate,agent)
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
+
 
 def experiment_3():
   learning_rate = 0.3
   discount_rate = 0.5
 
+  pickup_states = [[0, 0], [2, 2], [4, 4]]
+  dropoff_states = [[1, 4], [4, 0], [4, 2]]
+
+  initalizeCells(pickup_states, dropoff_states)
+
   agent.policy = "PRandom"
-  next_action = SARSA_update(learning_rate, discount_rate, None, agent)
+  next_action = SARSA_update(learning_rate, discount_rate, None, agent, pickup_states, dropoff_states)
   for index in range(200):
-    next_action = SARSA_update(learning_rate, discount_rate, next_action,agent)
+    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states)
 
   # TODO Display and interpret the Q-table
 
   for index in range(7800):
     agent.policy = "PExploit"
-    next_action = SARSA_update(learning_rate, discount_rate, next_action ,agent)
+    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states)
 
   #TODO final Q_table
 
@@ -343,23 +310,43 @@ def experiments_4():
   learning_rate = 0.3
   discount_rate = 1
 
+  pickup_states = [[0, 0], [2, 2], [4, 4]]
+  dropoff_states = [[1, 4], [4, 0], [4, 2]]
+
+  initalizeCells(pickup_states, dropoff_states)
+
   agent.policy = "PRandom"
-  next_action = SARSA_update(learning_rate, discount_rate, None, agent)
+  next_action = SARSA_update(learning_rate, discount_rate, None, agent, pickup_states, dropoff_states)
   for index in range(200):
-    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent)
+    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states)
 
   # TODO Display and interpret the Q-table
 
   for index in range(7800):
     agent.policy = "PExploit"
-    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent)
+    next_action = SARSA_update(learning_rate, discount_rate, next_action, agent, pickup_states, dropoff_states)
 
   # TODO final Q_table
 
 
 def experiment_5():
-  pass
+  learning_rate = 0.3
+  discount_rate = 0.5
 
+  pickup_states =  [[1, 4], [4, 0], [4, 2]]
+  dropoff_states = [[0, 0], [2, 2], [4, 4]]
+
+  initalizeCells(pickup_states, dropoff_states)
+
+  for index in range(200):
+    agent.policy = "PRandom"
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
+
+  # TODO Display and interpret the Q-table
+
+  for index in range(7800):
+    agent.policy = "PExploit"
+    Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
 
 
 #--------- MAIN ----------#
