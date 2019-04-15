06b27491ab79b9bfdb74cf18a7607e97ca0b362a
aAlejandroZ <azalejandro1@yahoo.com>
2019-04-15 16:43:41 -0500

    We can select multiple experiments with spacebar

--- gridworld.py
+++ gridworld.py
@@ -10,16 +10,13 @@ class GridWorld(Frame):
     self.master = master
     Frame.__init__(self, self.master)
     self.c = Canvas(self.master, height=500, width=500, bg='black')
-    self.sr = []
     self.agent_data = {"x": 0, "y": 0, "item": None}
     self.create_grid()
     self.num = 1
     self.update_gird_numbs()
     self.agent = ()
     self.create_agent()
-    self.c.bind("<Key>", self.key)
-    self.c.bind("<Button-1>", self.callback)
-    self.master.bind("<space>", lambda e: self.experiment_1())
+    self.master.bind("<space>", lambda e: self.prompt_experiments())
     self.c.pack(fill=BOTH, expand=True)
 
   def create_grid(self):
@@ -55,15 +52,6 @@ class GridWorld(Frame):
     for i in range(0, 600, 100):
       self.c.create_line([(0, i), (w, i)], tag='grid_line', fill='white')
 
-  def init_zeros(self):
-    s = 0
-    x = 25
-    y = 5
-    self.sr = np.zeros([x, y], dtype=np.object)
-    for x in range(25):
-      self.sr[:, 1:] = float(0)
-      self.sr[x][0] = s
-      s = s + 1
 
   def update_gird_numbs(self):
     global pickup_q_table
@@ -143,12 +131,6 @@ class GridWorld(Frame):
     self.agent = self.c.create_oval(434, 34, 464, 64, fill='cyan', tags='Agent')
     self.agent_data["item"] = self.c.find_closest(448, 47)[0]
 
-  def key(self, event):
-    print("pressed", repr(event.char))
-
-  def callback(self, event):
-    print("clicked at", event.x, event.y)
-
   def moveAndUpdateAgent(self):
     # time.sleep(0.2)
     self.move_agent(agent.action)
@@ -174,18 +156,24 @@ class GridWorld(Frame):
     for index in range(4000):
       agent.policy = "PRandom"
       Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
-      self.moveAndUpdateAgent()
+      time.sleep(0.5)
+      self.move_agent(agent.action)
+      self.delete_nums()
+      self.update_gird_numbs()
+      self.master.update()
 
     # TODO Where we display the Q_table
 
     for index in range(4000):
       agent.policy = "PGreedy"
       Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
-      self.moveAndUpdateAgent()
+      time.sleep(0.5)
+      self.move_agent(agent.action)
+      self.delete_nums()
+      self.update_gird_numbs()
+      self.master.update()
 
     print("FINISH")
-
-    initialize_Q_table()
 
   def experiment_2(self):
     learning_rate = 0.3
@@ -199,40 +187,72 @@ class GridWorld(Frame):
     for index in range(200):
       agent.policy = "PRandom"
       Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
-      self.moveAndUpdateAgent()
+      # time.sleep(0.35)
+      self.move_agent(agent.action)
+      self.delete_nums()
+      self.update_gird_numbs()
+      self.master.update()
 
     for index in range(7800):
       agent.policy = "PExploit"
       Q_learning(learning_rate, discount_rate, agent, pickup_states, dropoff_states)
-      self.moveAndUpdateAgent()
+      # time.sleep(0.35)
+      self.move_agent(agent.action)
+      self.delete_nums()
+      self.update_gird_numbs()
+      self.master.update()
 
     print("FINISH")
+
 
     initialize_Q_table()
 
   def move_agent(self, action):
-
     if action.name == "NORTH":
-      self.c.move(self.agent_data['item'], 0, -100)
+      self.c.move(self.agent, 0, -100)
     elif action.name == "EAST":
-      self.c.move(self.agent_data['item'], 100, 0)
+      self.c.move(self.agent, 100, 0)
     elif action.name == "SOUTH":
-      self.c.move(self.agent_data['item'], 0, 100)
+      self.c.move(self.agent, 0, 100)
     elif action.name == "WEST":
-      self.c.move(self.agent_data['item'], -100, 0)
-    elif action.name == "RESET":
-      self.resetAgent()
+      self.c.move(self.agent, -100, 0)
+    # elif action.name == "RESET":
+    #   self.resetAgent()
 
     self.master.update()
 
   def delete_nums(self):
     self.c.delete("nums")
 
+  def prompt_experiments(self):
+    experiment_num = int(input("Choose Experiment 1 - 5 by entering the corresponding number -  \n" ))
+    if experiment_num == 1:
+      print("|----------------Running Experiment 1----------------| \n")
+      self.experiment_1()
+      initialize_Q_table()
+      self.create_agent()
+      self.delete_nums()
+    elif experiment_num == 2:
+      print("|----------------Running Experiment 2----------------|\n")
+      self.experiment_2()
+      initialize_Q_table()
+      self.create_agent()
+      self.delete_nums()
+    # elif experiment_num == 3:
+    #   print("|----------------Running Experiment 3----------------|\n")
+    #   experiment_3()
+    # elif experiment_num == 4:
+    #   print("|----------------Running Experiment 4----------------|\n")
+    #   experiments_4()
+    # elif experiment_num == 5:
+    #   print("|----------------Running Experiment 5----------------|\n")
+    #   experiment_5()
+
 
 class Main(GridWorld):
   def __init__(self):
     master = Tk()
-
+    
     master.resizable(width=False, height=False)
 
     app = GridWorld(master)
