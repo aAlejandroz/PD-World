e0aa827f2abde0b9fabe4425441dca0fea0ee734
o_cas <o_castaneda@outlook.com>
2019-04-16 18:16:29 -0500

    - experiments 4 & 5

---
+++ test.py
@@ -0,0 +1,6 @@
+# elif experiment_num == 4:
+    #   print("|----------------Running Experiment 4----------------|\n")
+    #   experiments_4()
+    # elif experiment_num == 5:
+    #   print("|----------------Running Experiment 5----------------|\n")
+    #   experiment_5()
